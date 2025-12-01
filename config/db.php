1073df6a3b7bd97c0ae5c1fd11abde47c46a827b
taziyahcharles <taziyahcharles@gmail.com>
2025-12-01 13:09:54 -0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,20 +1,12 @@
-
 <?php
-$host = 'mysql-125f1ed0-taziyahcharles-c5cc.g.aivencloud.com';
-$db   = 'adoption';
-$user = 'avnadmin';
-$pass = 'AVNS_0YEDaM4bT-koB69Fqv8';
-$charset = 'utf8mb4';
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$name = getenv("DB_NAME");
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$conn = new mysqli($host, $user, $pass, $name);
 
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-];
-
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+?>
